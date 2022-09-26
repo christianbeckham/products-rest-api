c88b4adf1a3d0674c6c8b7587ab4e462d8b46194
Christian Beckham <urenacb22@gmail.com>
2022-09-26 14:41:36 -0500

    feat: updated urls.py and created function to view all product reviews

--- product_reviews/views.py
+++ product_reviews/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Review
+from .serializers import ReviewSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def reviews_list(request):
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
