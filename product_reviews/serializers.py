629a08b1a5f4f8f1eac40e9e5e2ac8942772ca3b
Christian Beckham <urenacb22@gmail.com>
2022-09-26 11:40:27 -0500

    feat: created products_review app, created Review model, created serilizers.py and Review serializer model, created urls.py file, and registered app with the admin site

---
+++ product_reviews/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Review
+
+# Create your serializers here.
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Review
+        fields = ['id', 'review_text', 'rating', 'name', 'product']
