d2ef6b9a8a69a56ad011f1dd4c44f6c867057c07
Christian Beckham <urenacb22@gmail.com>
2022-09-26 15:08:11 -0500

    feat: added functionality to get a review by id

--- product_reviews/urls.py
+++ product_reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reviews_list)
+    path('', views.reviews_list),
+    path('<int:pk>/', views.reviews_detail)
 ]
--- product_reviews/views.py
+++ product_reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,3 +19,11 @@ def reviews_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def reviews_detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data, status=status.HTTP_200_OK)
