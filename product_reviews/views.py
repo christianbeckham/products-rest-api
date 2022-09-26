23cf2dee3982a98b96e295da8f18e1fc95a22f6c
Christian Beckham <urenacb22@gmail.com>
2022-09-26 15:00:30 -0500

    feat: added functionality to create a product review

--- product_reviews/views.py
+++ product_reviews/views.py
@@ -8,9 +8,14 @@ from .serializers import ReviewSerializer
 # Create your views here.
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def reviews_list(request):
     if request.method == 'GET':
         reviews = Review.objects.all()
         serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
